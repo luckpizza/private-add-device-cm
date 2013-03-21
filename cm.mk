6aa8b99840c6eb479eacf61939b54ac9d40a2a7f
Lucas <lucas@lucas-xompu.(none)>
2013-03-21 12:46:39 +0100

    adding screen configuration and cleaning unneeded

--- cm.mk
+++ cm.mk
@@ -1,14 +1,17 @@
 # Release name
 PRODUCT_RELEASE_NAME := SEDPRETTYNAME
 
-# Boot animation
-TARGET_BOOTANIMATION_NAME := horizontal-1024x768
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+#If you need gsm compatibility
+$(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/SEDMANUFACTURER/SEDNAME/full_SEDNAME.mk)
+
+#### Set screen size
+TARGET_SCREEN_HEIGHT := 768
+TARGET_SCREEN_WIDTH := 1024
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := SEDNAME
@@ -18,4 +21,4 @@ PRODUCT_MODEL := SEDPRETTYNAME
 PRODUCT_MANUFACTURER := SEDMANUFACTURER
 
 #Set build fingerprint / ID / Product Name etc
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SEDNAME BUILD_FINGERPRINT="SEDMANUFACTURER/SEDNAME/SEDNAME:4.0.4/IMM76I/330937:user/release-keys" PRIVATE_BUILD_DESC="SEDNAME-user 4.0.4 IMM76I 330937 release-keys"
+#PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SEDNAME BUILD_FINGERPRINT="SEDMANUFACTURER/SEDNAME/SEDNAME:4.0.4/IMM76I/330937:user/release-keys" PRIVATE_BUILD_DESC="SEDNAME-user 4.0.4 IMM76I 330937 release-keys"
