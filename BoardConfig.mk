e956c3da47d5356932501ae5470201cee7406d81
Lucas <lucas@lucas-xompu.(none)>
2013-03-21 14:35:41 +0100

    generalize files + using tp971500dc as example

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,15 +23,6 @@
 
 
 
-#IF using a prebuilt kernel
-TARGET_PREBUILT_KERNEL:=device/SEDMANUFACTURER/SEDNAME/kernel
-#IF compileing the kernel
-#TARGET_KERNEL_SOURCE := PUT_THE_PATH_TO_KERNEL_SOURCE
-#TARGET_KERNEL_CONFIG := SEDNAME_defconfig
-#TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-linux-gnueabi-
-
-
-## Plase change if CPU ABI is different!
 TARGET_CPU_ABI:=armeabi-v7a
 TARGET_CPU_ABI2:=armeabi
 TARGET_CPU_SMP:=true
@@ -45,6 +36,72 @@ ARCH_ARM_HAVE_ARMV7A:=true
 TARGET_GLOBAL_CFLAGS+=-mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS+=-mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
 
-#Dont build a BOOTLOADER
+#IF using a prebuilt kernel
+TARGET_PREBUILT_KERNEL:=device/SEDMANUFACTURER/SEDNAME/kernel
+#IF compileing the kernel
+#TARGET_KERNEL_SOURCE := kernel/rk/rk30sdk
+#TARGET_KERNEL_CONFIG := SEDNAME_defconfig
+#TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-linux-gnueabi-
+BOARD_KERNEL_BASE:=0x60400000
+BOARD_KERNEL_CMDLINE:=
+BOARD_PAGE_SIZE:=16384
+TARGET_NO_RADIOIMAGE:=true
+TARGET_BOARD_PLATFORM:=rk30sdk
+TARGET_BOOTLOADER_BOARD_NAME:=rk30board
+
+
+
+
+# FILESYSTEM
+BOARD_HAS_LARGE_FILESYSTEM:=true
+TARGET_USERIMAGES_USE_EXT4:=true
+BOARD_SYSTEMIMAGE_PARTITION_SIZE:=524288000
+BOARD_FLASH_BLOCK_SIZE:=131072
+BOARD_USERDATAIMAGE_PARTITION_SIZE:=1572864000
+
+# MISC
+TARGET_PROVIDES_INIT_RC:=true
+TARGET_BOOTANIMATION_PRELOAD:=true
+
+# GENERIC
+BOARD_USES_GENERIC_AUDIO := true
+USE_CAMERA_STUB := true
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+TARGET_USES_ION := true
+BOARD_USES_HC_RADIO := true
 TARGET_NO_BOOTLOADER := true
+
+# OVERLAY
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# GRAPHICS FIX
+BOARD_EGL_CFG := device/SEDMANUFACTURER/SEDNAME/prebuilt/configs/egl.cfg
+USE_OPENGL_RENDERER := true
+ENABLE_WEBGL := true
+BOARD_USE_SKIA_LCDTEXT := true
+TARGET_DISABLE_TRIPLE_BUFFERING := true
+
+# RECOVERY
+#TARGET_RECOVERY_INITRC := device/SEDMANUFACTURER/SEDNAME/recovery.init.rc
+
+
+#BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
+#BOARD_UMS_2ND_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun1/file"
+
+# BLUETOOTH
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# WIFI
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE           := bcmdhd
+WIFI_DRIVER_FW_PATH_STA     := "/system/etc/firmware/fw_bcm4329.bin"
+WIFI_DRIVER_FW_PATH_P2P     := "/system/etc/firmware/fw_bcm4329_p2p.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/system/etc/firmware/fw_bcm4329_apsta.bin"
+
+
+#This should stay always
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/SEDMANUFACTURER/SEDNAME/releasetools/ota_from_target_files
 
--- full_SEDNAME.mk
+++ full_SEDNAME.mk
@@ -12,18 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#
-# This file is the build configuration for a full Android
-# build for zatab hardware. This cleanly combines a set of
-# device-specific aspects (drivers) with a device-agnostic
-# product configuration (apps). Except for a few implementation
-# details, it only fundamentally contains two inherit-product
-# lines, full and zatab, hence its name.
-#
-
-# Camera
-PRODUCT_PACKAGES := \
-    Camera
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
@@ -32,9 +20,157 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit from our device
 $(call inherit-product, device/SEDMANUFACTURER/SEDNAME/device.mk)
 
-# Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_SEDNAME
+
+
+# include device/SEDMANUFACTURER/rk30_common/BoardConfigCommon.mk
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+DEVICE_PACKAGE_OVERLAYS := device/SEDMANUFACTURER/SEDNAME/overlay
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+    LOCAL_KERNEL := device/SEDMANUFACTURER/SEDNAME/kernel
+else
+    LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+# AUDIO LIB
+ifeq ($(strip $(BOARD_USES_ALSA_AUDIO)),true)
+PRODUCT_PACKAGES += \
+    libasound \
+    alsa.default \
+    acoustics.default
+endif
+
+# RAMDISK
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/ramdisk/rk30xxnand_ko.ko:root/rk30xxnand_ko.ko \
+    $(LOCAL_PATH)/prebuilt/ramdisk/init:root/init \
+    $(LOCAL_PATH)/prebuilt/ramdisk/init.rc:root/init.rc \
+    $(LOCAL_PATH)/prebuilt/ramdisk/init.rk30board.rc:root/init.rk30board.rc \
+    $(LOCAL_PATH)/prebuilt/ramdisk/init.rk30board.usb.rc:root/init.rk30board.usb.rc \
+    $(LOCAL_PATH)/prebuilt/ramdisk/misc.img:root/misc.img \
+    $(LOCAL_PATH)/prebuilt/ramdisk/ueventd.rk30board.rc:root/ueventd.rk30board.rc
+
+# init.d scripts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/init.d/03kernel:system/etc/init.d/03kernel \
+    $(LOCAL_PATH)/prebuilt/init.d/04mount:system/etc/init.d/04mount
+# Prebuilt configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/configs/call-pppd:system/etc/ppp/call-pppd \
+    $(LOCAL_PATH)/prebuilt/configs/ip-down:system/etc/ppp/ip-down \
+    $(LOCAL_PATH)/prebuilt/configs/ip-up:system/etc/ppp/ip-up \
+    $(LOCAL_PATH)/prebuilt/configs/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/prebuilt/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/prebuilt/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/prebuilt/configs/vold.fstab:system/etc/vold.fstab \
+    $(LOCAL_PATH)/prebuilt/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    $(LOCAL_PATH)/prebuilt/configs/rk29-keypad.kl:/system/usr/keylayout/rk29-keypad.kl
+
+# Prebuilt kernel modules
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/modules/mali.ko:system/lib/modules/mali.ko \
+    $(LOCAL_PATH)/prebuilt/modules/rk29-ipp.ko:system/lib/modules/rk29-ipp.ko \
+    $(LOCAL_PATH)/prebuilt/modules/rkwifi.ko:system/lib/modules/rkwifi.ko \
+    $(LOCAL_PATH)/prebuilt/modules/ump.ko:system/lib/modules/ump.ko \
+    $(LOCAL_PATH)/prebuilt/modules/vpu_service.ko:system/lib/modules/vpu_service.ko \
+    $(LOCAL_PATH)/prebuilt/modules/wlan.ko:system/lib/modules/wlan.ko
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/configs/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+
+PRODUCT_PACKAGES := \
+    HoloSpiralWallpaper \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+
+PRODUCT_PACKAGES += \
+    Camera
+
+PRODUCT_PACKAGES += \
+    audio.primary.default \
+    audio_policy.default
+
+PRODUCT_PACKAGES += \
+    librs_jni \
+    com.android.future.usb.accessory
+
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    setup_fs
+
+PRODUCT_PROPERTY_OVERRIDES := \
+    hwui.render_dirty_regions=false \
+    service.adb.root=1 \
+    ro.secure=0 \
+    ro.allow.mock.location=1 \
+    ro.debuggable=1
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.locationfeatures=1 \
+    ro.setupwizard.enable_bypass=1 \
+    dalvik.vm.execution-mode=int:jit \
+    dalvik.vm.lockprof.threshold=500 \
+    dalvik.vm.dexopt-flags=m=y
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hdmi.enable=true \
+    persist.sys.timezone=Europe/Berlin \
+    persist.sys.language=de \
+    persist.sys.country=DE \
+    persist.sys.use_dithering=1 \
+    persist.sys.purgeable_assets=0 \
+    windowsmgr.max_events_per_sec=240 \
+    view.touch_slop=2 \
+    view.minimum_fling_velocity=25 \
+    ro.additionalmounts=/storage/sdcard0 \
+    ro.vold.switchablepair=/storage/sdcard0,/storage/sdcard1 \
+    persist.sys.vold.switchexternal=0
+
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+PRODUCT_AAPT_CONFIG := large mdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+
+PRODUCT_CHARACTERISTICS := tablet
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+
+PRODUCT_NAME := cm_SEDNAME
 PRODUCT_DEVICE := SEDNAME
 PRODUCT_BRAND := SEDBRANDING
 PRODUCT_MODEL := SEDPRETTYNAME
 PRODUCT_MANUFACTURER := SEDMANUFACTURER
+
+# WIFI
+#$(call inherit-product, external/wlan_loader/wifi-firmware.mk)
+
+# BLUETOOTH
+#$(call inherit-product, system/bluetooth/brcm_patchram_plus/hcd.mk)
+
+#$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
+#$(call inherit-product, build/target/product/full_base.mk)
+#$(call inherit-product-if-exists, vendor/SEDMANUFACTURER/SEDNAME/tp971500dc-vendor.mk)
+
