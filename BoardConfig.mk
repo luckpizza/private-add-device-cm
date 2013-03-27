afa6efa10e859ca2e5dc889bc630fd00287f61e6
Lucas Pizzagalli <luckpizza@gmail.com>
2013-03-27 14:16:34 +0100

    removing unnecesary inhereits and adding CPU ABI

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,13 +16,35 @@
 -include vendor/SEDMANUFACTURER/SEDNAME/BoardConfigVendor.mk
 
 # Use the part that is common between all SEDMANUFACTURER
-include device/SEDMANUFACTURER/common/BoardConfig.mk
+#include device/SEDMANUFACTURER/common/BoardConfig.mk
 
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/SEDMANUFACTURER/SEDNAME/recovery_keys.c
+## Use to map recoery keys
+#BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/SEDMANUFACTURER/SEDNAME/recovery_keys.c
 
-TARGET_KERNEL_CONFIG := SEDNAME_defconfig
 
-SW_BOARD_USES_GSENSOR_TYPE := bma250
-SW_BOARD_GSENSOR_XY_REVERT := true
-SW_BOARD_GSENSOR_DIRECT_X := true
-SW_BOARD_GSENSOR_DIRECT_Y := true
+
+#IF using a prebuilt kernel
+TARGET_PREBUILT_KERNEL:=device/SEDMANUFACTURER/SEDNAME/kernel
+#IF compileing the kernel
+#TARGET_KERNEL_SOURCE := PUT_THE_PATH_TO_KERNEL_SOURCE
+#TARGET_KERNEL_CONFIG := SEDNAME_defconfig
+#TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-linux-gnueabi-
+
+
+## Plase change if CPU ABI is different!
+TARGET_CPU_ABI:=armeabi-v7a
+TARGET_CPU_ABI2:=armeabi
+TARGET_CPU_SMP:=true
+TARGET_ARCH_VARIANT:=armv7-a-neon
+ARCH_ARM_HAVE_VFP:=true
+TARGET_ARCH_VARIANT_CPU:=cortex-a9
+TARGET_ARCH_VARIANT_FPU:=neon
+ARCH_ARM_HAVE_NEON:=true
+ARCH_ARM_HAVE_TLS_REGISTER:=true
+ARCH_ARM_HAVE_ARMV7A:=true
+TARGET_GLOBAL_CFLAGS+=-mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS+=-mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
+
+#Dont build a BOOTLOADER
+TARGET_NO_BOOTLOADER := true
+
