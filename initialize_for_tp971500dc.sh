718c597b6c05564e82d284efce08ad05145956f1
Lucas Pizzagalli <luckpizza@gmail.com>
2013-03-27 19:39:06 +0100

    copy the externas and patches to the right place

--- initialize_for_tp971500dc.sh
+++ initialize_for_tp971500dc.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 ./initialize.sh tp971500dc  TP9,7-1500DC-metal Ionik rockchip
+ cp -a patches/system/bluetooth/brcm_patchram_plus/  ../../../system/bluetooth/
+ cp -a external/wlan_loader/  ../../../external/
