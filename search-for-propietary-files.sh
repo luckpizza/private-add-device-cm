7e04beb7e377722b074b5b377edce852ed4d2ae2
Lucas Pizzagalli <luckpizza@gmail.com>
2013-04-03 19:21:11 +0200

    new improved search propietary files, search is more specific

--- search-for-propietary-files.sh
+++ search-for-propietary-files.sh
@@ -5,9 +5,31 @@ if [ $# -le 1 ] ||  [ ! -d $1 ] ||  [ ! -d $2 ]  ; then
         exit -1
 fi
 
+rm  proprietary-files.txt
+echo "##lib files" >> proprietary-files.txt
+echo "">> proprietary-files.txt
 
-diff <(cd $1 && find | sort) <(cd $2 && find | sort) > proprietary-files.txt
+diff <(cd $1 && find lib/ | sort) <(cd $2 && find lib/ | sort) >> proprietary-files.txt
+
+echo "">> proprietary-files.txt
+echo "##vendor files" >> proprietary-files.txt
+echo "">> proprietary-files.txt
+
+diff <(cd $1 && find vendor/ | sort) <(cd $2 && find vendor/ | sort) >> proprietary-files.txt
+
+echo "">> proprietary-files.txt
+echo "##firmware configuration files" >> proprietary-files.txt
+echo "">> proprietary-files.txt
+
+diff <(cd $1 && find etc/firmware/ | sort) <(cd $2 && find etc/firmware/ | sort) >> proprietary-files.txt
+
+echo "">> proprietary-files.txt
+echo "##binary files" >> proprietary-files.txt
+echo "">> proprietary-files.txt
+
+diff <(cd $1 && find bin/ | sort) <(cd $2 && find bin/ | sort) >> proprietary-files.txt
 sed -i '/^[0-9]/d' proprietary-files.txt
 sed -i '/^>/d' proprietary-files.txt
-sed -i 's/< .\///' proprietary-files.txt
+sed -i '/---/d' proprietary-files.txt
+sed -i 's/< //' proprietary-files.txt
 
