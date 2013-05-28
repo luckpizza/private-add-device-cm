ba4cd7e65e895fec720b941b55dd04a673fcbbc2
Lucas Pizzagalli <luckpizza@gmail.com>
2013-05-28 15:43:12 +0200

    adding checks and removing auto git commiting

--- initialize.sh
+++ initialize.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
+
+if [ $# -le 3 ] ; then
+	echo "Please provide a valid directory to copy the files from"
+	exit -1
+fi
+
+
 cd $(dirname "$0")
 
 find . -type f -not -wholename "*.git*" -exec sed -i "s/SEDNAME/$1/g" {} \;
 find . -type f -not -wholename "*.git*" -exec sed -i "s/SEDPRETTYNAME/$2/g" {} \;
 find . -type f -not -wholename "*.git*" -exec sed -i "s/SEDBRANDING/$3/g" {} \;
 find . -type f -not -wholename "*.git*" -exec sed -i "s/SEDMANUFACTURER/$4/g" {} \;
-git add -A
 
-git mv full_SEDNAME.mk "full_$1.mk"
 
-git rm -f "$0" && git commit -m "Initial commit for device $2 by $3 manufactured by $4"
+mv full_SEDNAME.mk "full_$1.mk"
+
+echo "Initialization done for device $2 by $3 manufactured by $4"
